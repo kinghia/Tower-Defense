20ce7fb3b38cbc29a6fb328d1fc927e88750ae13
kinghia <162483327+kinghia@users.noreply.github.com>
2025-03-17 00:46:26 +0700

    17/3 update neighborCoords

--- Assets/Tile/CoordinateLabeler.cs
+++ Assets/Tile/CoordinateLabeler.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 
 [ExecuteAlways]
 [RequireComponent(typeof(TextMeshPro))]
@@ -10,17 +9,20 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
+
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
     void Update()
@@ -29,6 +31,7 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
+            label.enabled = true;
         }
 
         SetLabelColor();
@@ -45,13 +48,27 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetLabelColor()
     {
-        if (waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        }   
-        else 
+        if (gridManager == null) {return;}
+        
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) {return; }
+
+        if (!node.isWalkable)
         {
             label.color = blockedColor;
+        }
+        else if (node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+        else
+        {
+            label.color = defaultColor;
         }
     }
 
