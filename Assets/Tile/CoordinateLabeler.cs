cf39794819783b8912cb68aaa973a29299882564
kinghia <162483327+kinghia@users.noreply.github.com>
2025-03-20 00:27:19 +0700

    19/3 update scripts

--- Assets/Tile/CoordinateLabeler.cs
+++ Assets/Tile/CoordinateLabeler.cs
@@ -21,7 +21,7 @@ public class CoordinateLabeler : MonoBehaviour
     {
         gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
-        label.enabled = false;
+        label.enabled = true;
 
         DisplayCoordinates();
     }
@@ -74,8 +74,10 @@ public class CoordinateLabeler : MonoBehaviour
 
     void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        if (gridManager == null) {return;}
+
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / gridManager.UnityGridSize);
 
         label.text = coordinates.x + "," + coordinates.y;
     }
